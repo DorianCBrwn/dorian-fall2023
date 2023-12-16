cafe912f32edbbae9f417362bd429d0de3bc1bf1
DorianCBrwn <brown.3794@gmail.com>
2023-12-16 10:11:07 -0500

    add vpc file

---
+++ terraform-w13-hw/aws-config/security-group.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "ec2-security-group" {
+    name        = "terraform-security-group"
+    description = "Security group made by terraform"
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["var.my_ip"]
+    }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "example-security-group"
+    }
+}
--- terraform-w13-hw/aws-config/variables.tf
+++ terraform-w13-hw/aws-config/variables.tf
@@ -15,3 +15,15 @@ variable "key_name"{
     type        = string
     default     = "general-ec2"
 }
+
+variable "my_ip_cider"{
+    description = "The cider block for ec2"
+    type        = string
+    default     = "71.172.197.159/32"
+}
+
+variable "vpc_cider_block"{
+    description = "The cider block for vpc"
+    type        = string
+    default     = "10.0.0.0/16"
+}
