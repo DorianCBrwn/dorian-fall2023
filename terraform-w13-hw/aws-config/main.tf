22f7d810537ec1e93b35be775e1f947b6ffce862
DorianCBrwn <brown.3794@gmail.com>
2023-12-16 22:05:45 -0500

    Fix vpc_security_group_ids in aws_instance resource

--- terraform-w13-hw/aws-config/main.tf
+++ terraform-w13-hw/aws-config/main.tf
@@ -12,7 +12,7 @@ terraform {
 resource "aws_instance" "app_server" {
   ami                    = "ami-0fc5d935ebf8bc3bc"
   instance_type          = var.instance_type
-  vpc_security_group_ids = [ec2-security-group]
+  vpc_security_group_ids = [ec2-security-group.id]
   key_name               = var.key_name
 
   subnet_id = aws_subnet.public_subnets[0].id
