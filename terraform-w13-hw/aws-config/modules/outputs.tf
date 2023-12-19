f99398522b28e3ad3b10ba2f63aa7062242a388e
DorianCBrwn <brown.3794@gmail.com>
2023-12-19 15:36:36 -0500

    Update AWS instance variable names in outputs.tf

--- terraform-w13-hw/aws-config/modules/outputs.tf
+++ terraform-w13-hw/aws-config/modules/outputs.tf
@@ -1,28 +1,17 @@
 # Output variable definitions
 
 output "instance_id" {
-    value = aws_instance.example.id
+    value = aws_instance.ec2.id
 }
 
 output "instance_public_ip" {
-    value = aws_instance.example.public_ip
+    value = aws_instance.ec2.public_ip
 }
 
 output "instance_private_ip" {
-    value = aws_instance.example.private_ip
+    value = aws_instance.ec2.private_ip
 }
 
 output "instance_key_name" {
-    value = aws_instance.example.key_name
-}
-
-output "instance_security_group" {
-    value = aws_instance.example.security_group_names
-}
-output "instance_availability_zone" {
-    value = aws_instance.example.availability_zone
-}
-
-output "instance_subnet_id" {
-    value = aws_instance.example.subnet_id
+    value = aws_instance.ec2.key_name
 }
