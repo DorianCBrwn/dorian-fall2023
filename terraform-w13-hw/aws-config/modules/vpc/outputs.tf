5c735ddb271416f514ffbbc36794f95f8028b038
DorianCBrwn <brown.3794@gmail.com>
2023-12-22 14:12:37 -0500

    Add output for public subnets

--- terraform-w13-hw/aws-config/modules/vpc/outputs.tf
+++ terraform-w13-hw/aws-config/modules/vpc/outputs.tf
@@ -13,3 +13,6 @@ output "vpc_main_route_table_id" {
 output "vpc_default_network_acl_id" {
     value = aws_vpc.main_vpc.default_network_acl_id
 }
+output "public_subnets"{
+    value = aws_subnet.public_subnets[*].id
+}
