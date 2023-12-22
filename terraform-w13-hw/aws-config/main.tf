c5c9a120fa7fe9aa7951f892f460a98e6332415a
DorianCBrwn <brown.3794@gmail.com>
2023-12-22 12:32:22 -0500

    Refactor module names in main.tf and fix module references in ec2/main.tf and vpc/outputs.tf

--- terraform-w13-hw/aws-config/main.tf
+++ terraform-w13-hw/aws-config/main.tf
@@ -9,7 +9,7 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-module "ec2-vpc-module" {
+module "ec2-module" {
   source = "./modules/ec2"
 
 }
--- terraform-w13-hw/aws-config/modules/vpc/outputs.tf
+++ terraform-w13-hw/aws-config/modules/vpc/outputs.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
-    value = aws_vpc.main.id
+    value = module.vpc-module.vpc_id
 }
 
 output "vpc_cidr_block" {
-    value = aws_vpc.main.cidr_block
+    value = module.vpc_module.main.cidr_block
 }
 
 output "vpc_default_security_group_id" {
-    value = aws_vpc.main.default_security_group_id
+    value = module.vpc_module.main.default_security_group_id
 }
 
 output "vpc_main_route_table_id" {
-    value = aws_vpc.main.main_route_table_id
+    value = module.vpc_module.main.main_route_table_id
 }
 
 output "vpc_default_network_acl_id" {
-    value = aws_vpc.main.default_network_acl_id
+    value = module.vpc_module.main.default_network_acl_id
 }
