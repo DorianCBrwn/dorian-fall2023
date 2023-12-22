13dcf8be506daa8d9e5b168e51c973ee1c8e7ec1
DorianCBrwn <brown.3794@gmail.com>
2023-12-22 12:36:42 -0500

    Fix module name in vpc outputs

--- terraform-w13-hw/aws-config/modules/vpc/outputs.tf
+++ terraform-w13-hw/aws-config/modules/vpc/outputs.tf
@@ -3,17 +3,17 @@ output "vpc_id" {
 }
 
 output "vpc_cidr_block" {
-    value = module.vpc_module.main.cidr_block
+    value = module.vpc-module.main.cidr_block
 }
 
 output "vpc_default_security_group_id" {
-    value = module.vpc_module.main.default_security_group_id
+    value = module.vpc-module.main.default_security_group_id
 }
 
 output "vpc_main_route_table_id" {
-    value = module.vpc_module.main.main_route_table_id
+    value = module.vpc-module.main.main_route_table_id
 }
 
 output "vpc_default_network_acl_id" {
-    value = module.vpc_module.main.default_network_acl_id
+    value = module.vpc-module.main.default_network_acl_id
 }
