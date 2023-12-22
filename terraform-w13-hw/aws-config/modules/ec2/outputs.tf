f5ff39899296ea9f6be75a4c73ed9bd9bc790f06
DorianCBrwn <brown.3794@gmail.com>
2023-12-22 12:45:52 -0500

    Update output variable names in ec2 module

--- terraform-w13-hw/aws-config/modules/ec2/outputs.tf
+++ terraform-w13-hw/aws-config/modules/ec2/outputs.tf
@@ -1,17 +1,17 @@
 # Output variable definitions
 
 output "instance_id" {
-    value = module.ec2-module.ec2.id
+    value = ec2-instance.id
 }
 
 output "instance_public_ip" {
-    value = module.ec2-module.ec2.public_ip
+    value = ec2-instance.public_ip
 }
 
 output "instance_private_ip" {
-    value = module.ec2-module.ec2.private_ip
+    value = ec2-instance.private_ip
 }
 
 output "instance_key_name" {
-    value = module.ec2-module.ec2.key_name
+    value = ec2-instance.key_name
 }
